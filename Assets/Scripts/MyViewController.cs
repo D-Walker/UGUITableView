c32a382222bc725fd53189c33be0a33966473e84
D-Walker <905545097@qq.com>
2017-01-22 08:38:38 +0800

    添加注释

--- Assets/Scripts/MyViewController.cs
+++ Assets/Scripts/MyViewController.cs
@@ -11,13 +11,16 @@ public class MyViewController : MonoBehaviour {
     List<int> dataList = new List<int>();
 
     void Awake() {
+        //一共有多少行数据，t==tbView，下同
         tbView.delegateNumberOfCells = (t) => {
             return dataList.Count;
         };
+        //第i行的cell的高度
         tbView.delegateSizeOfIndex = (t, i) => {
             //奇数行和偶数行分别显示不同的高度，对应不同的样式
             return i % 2 == 0 ? 160 : 320;
         };
+        //返回第i行的cell（RectTransform）
         tbView.delegateTransformOfIndex = (t, i) => {
             //先从缓存队列中访问第i行是否有可用的cell
             RectTransform cell = t.DequeueReusabelCell(i);
@@ -39,6 +42,7 @@ public class MyViewController : MonoBehaviour {
             }
             return cell;
         };
+        //字符标识不同的cell样式
         tbView.delegateIdentifierOfIndex = (t, i) => {
             //奇数行和偶数行分别显示不同的高度，对应不同的样式。这里的字符串分别用来标识不同的样式，方便Dequeue时查找对应的cell
             return i % 2 == 0 ? "Short" : "High";
@@ -52,6 +56,7 @@ public class MyViewController : MonoBehaviour {
         tbView.ReloadData(-1);
 	}
 	
+    //每行数据加100
     public void OnAdd100() {
         for (int i = 0; i < 100; i++) {
             dataList[i] += 100;
@@ -59,10 +64,12 @@ public class MyViewController : MonoBehaviour {
         tbView.ReloadData(-2);
     }
 
+    //滚到顶部
     public void OnToTop() {
         tbView.ReloadData(-1);
     }
 
+    //滚到第51行
     public void OnTo50() {
         tbView.ReloadData(51);
     }
